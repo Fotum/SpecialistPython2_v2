c8c620f7336ce6a0dc9ea5a979d18099fd23b8d4
Fotum <MaDMaDDneSS@gmail.com>
2022-07-25 13:18:10 +0300

    Update 03_point_method.py
    Соколов Юрий

--- Module1/practice/points/03_point_method.py
+++ Module1/practice/points/03_point_method.py
@@ -1,18 +1,25 @@
+import math
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def dist_to(self, other_point):
-        # TODO-1: реализуйте метод
-        # Подсказка: смотри пример, Module1/examples/06_object_methods.py
-        ...
+    def distance(self, other):
+        """
+        Расстояние между двумя точками
+        """
+        return math.sqrt(
+            math.pow((other.x - self.x), 2) +
+            math.pow((other.y - self.y), 2)
+        )
 
 
-# Дано две точки на координатной плоскости
+# Даны две точки на координатной плоскости
 point1 = Point(2, 4)
 point2 = Point(5, -2)
 
-# TODO-2: выведите расстояние между точками используя метод dist_to()
-print(f"Расстояние между точками = {...}")
+dist = point1.distance(point2)
 
+print("Расстояние между точками = ", dist)
